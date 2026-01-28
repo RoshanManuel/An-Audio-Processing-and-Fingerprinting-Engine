822701b07e012ad20b70399a5361479f5513afc8
E.Roshan Manuel <roshanmanuel05@gmail.com>
2026-01-28 23:24:02 +0530

    Refactor spectrogram.h to use complexf.h and float

--- spectrogram.h
+++ spectrogram.h
@@ -2,14 +2,13 @@
 #define SHAZAM_SPECTROGRAM_H
 #include <cstdint>
 #include <vector>
-#include <complex>
-using cd = std::complex<double>;
+#include "complexf.h"
 typedef struct {
     uint8_t red, green, blue;
 }RGB;
-std::vector<std::vector<double>> SpectroCalc(const std::vector<std::vector<cd>>&);
-std::vector<std::vector<double>> magnitudeConvert(const std::vector<std::vector<double>>&);
-std::vector<std::vector<uint8_t>> normalize(const std::vector<std::vector<double>>&);
+//std::vector<std::vector<double>> SpectroCalc(const std::vector<std::vector<cf>>&);
+//std::vector<std::vector<double>> magnitudeConvert(const std::vector<std::vector<double>>&);
+std::vector<std::vector<uint8_t>> normalize(const std::vector<std::vector<float>>&);
 RGB jet(const uint8_t value);
 bool writePPM(const std::vector<std::vector<uint8_t>>& ,const char*);
 #endif
