fda738a1762714636728d756fb5531c5013a0eef
E.Roshan Manuel <roshanmanuel05@gmail.com>
2026-01-28 23:17:52 +0530

    Optimised downsampler
    Inlined and precomputed the hann table and twiddle factor table  
    Went with power instead of magnitude to cut down on finding square root  
    Removed all other unrequired functions

--- downsampler.cpp
+++ downsampler.cpp
@@ -1,30 +1,53 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <array>
 #include "downsampler.h"
 #include <complex>
-# define PI  3.14159265358979323846
-using cd  = std::complex<double>;
-
-std::vector<double> generateHannWindow(int windowSize) {
-    std::vector<double> hannWindow(windowSize,0.0);
+constexpr cf twiddle(const int k,const int N) {
+    const float angle = -2.0f * PI * k /N;
+    return {
+    __builtin_cosf(angle),__builtin_sinf(angle)};
+}
+inline constexpr std::array<std::array<cf,levels>,stages> buildTable() {
+    std::array<std::array<cf,levels>,stages> table{};
+    for (int i=0;i<stages;i++) {
+        const int N = 1 << (i+1);
+        for (int j=0;j<N/2;j++) {
+            table[i][j] = twiddle(j,N);
+        }
+    }
+    return table;
+}
+static constexpr std::array<std::array<cf,levels>,stages> table = buildTable();
+inline constexpr std::array<float,windowSize> generateHannWindowforSTFT() {
+    std::array<float,1024> hannWindow{};
     for (int i{}; i < windowSize; ++i) {
-        const double w = 0.5*(1.0-cos((2*PI*i)/(windowSize-1)));
+        const float w = 0.5f*(1.0f-__builtin_cosf((2*PI*i)/(windowSize-1.0f)));
         hannWindow[i] = w;
     }
     return hannWindow;
 }
-std::vector<double> lowpassfir(int cutoff,int originalRate,int filLen) {
-std::vector<double> h(filLen);
-    std::vector<double> w = generateHannWindow(filLen);
-const double fc = (cutoff*1.0)/originalRate;
+static constexpr auto hannWindow1024 = generateHannWindowforSTFT();
+inline constexpr std::array<float,65> generateHannWindowforLPF() {
+    std::array<float,filterTaps> hannWindow{};
+    for (int i{}; i <filterTaps; ++i) {
+        const float w = 0.5f*(1.0f-__builtin_cosf((2*PI*i)/(filterTaps-1.0f)));
+        hannWindow[i] = w;
+    }
+    return hannWindow;
+}
+static constexpr auto hannWindow65 = generateHannWindowforLPF();
+std::vector<float> lowpassfir(int cutoff,int originalRate,int filLen) {
+std::vector<float> h(filLen);
+const float fc = (cutoff*1.0f)/originalRate;
     const int N = filLen-1;
     for (int n{};n<=N;n++) {
         const int idx = n - N/2;
-        double h_ideal = (idx==0)?2*fc:(sin(2*PI*fc*idx)/(PI*idx));
-        h[n] = h_ideal * w[n];
+        float h_ideal = (idx==0)?2*fc:(sin(2.0f*PI*fc*idx)/(PI*idx));
+        h[n] = h_ideal * hannWindow65[n];
     }
-    double sum = 0.0;
+    float sum = 0.0;
     for (const auto& x : h ) {
         sum += x;
     }
@@ -33,14 +56,14 @@ const double fc = (cutoff*1.0)/originalRate;
     }
 return h;
 }
-std::vector<double> polyphaseDecimate(const std::vector<double>& signal,const std::vector<double>& h,int M) {
+std::vector<float> polyphaseDecimate(const std::vector<float>& signal,const std::vector<float>& h,int M) {
     const int N = signal.size();
     const int P  = h.size();
     int outSize = N/M;
-    std::vector<double> output(outSize,0.0);
+    std::vector<float> output(outSize,0.0);
     for (int i{}; i < outSize; ++i) {
         int n = i * M; // input index for this output sample
-        double sum = 0.0;
+        float sum = 0.0;
         const int k_start = std::max(0, n - N + 1);
         const int k_end = std::min(P, n + 1);
         for (int k = k_start; k < k_end; ++k) { // Convolve:
@@ -50,7 +73,7 @@ std::vector<double> polyphaseDecimate(const std::vector<double>& signal,const st
     }
     return output;
 }
-void iterativeFFT(std::vector<cd>& signal) {
+void iterativeFFT(std::vector<cf>& signal) {
     int n = signal.size();
     for (int i = 1, j = 0; i < n; i++) {
         int bit = n >> 1;
@@ -61,36 +84,46 @@ void iterativeFFT(std::vector<cd>& signal) {
         if (i < j)
             std::swap(signal[i], signal[j]);//only when not equal
     }
+    int stage = 0;
     for (int len = 2;len<=n;len<<=1) {
-        const double theta = -(2*PI)/len;
-        cd wl(cos(theta),sin(theta));
+        const cf* w = table[stage].data();
+        const int half = len >> 1;
         for (int i =0;i<n;i+=len) {
-            cd w(1); //phase rotate twiddle factor
-            for (int j=0;j<len/2;j++) { //butterfly step
-                cd u = signal[i+j];
-                cd v = signal[i+j+len/2]*w;
-                signal[i+j] = u+v;
-                signal[i+j+len/2] = u-v;
-                w*=wl;
+            for (int j=0;j<half;j++) { //butterfly step
+                const cf a = signal[i+j];
+                const cf b = signal[i+j+half];
+                const cf wj = w[j];
+                const float c_real = b.real * wj.real - b.imag * wj.imag;
+                const float c_imag  = b.real * wj.imag + b.imag * wj.real;
+                signal[i+j] =  {a.real+c_real,a.imag+c_imag};
+                signal[i+j+half] =  {a.real-c_real,a.imag-c_imag};
             }
         }
+        stage++;
     }
 }
-std::vector<std::vector<cd>> signalFramer(const std::vector<double>& signal,const int frameSize,const int hopSize) {
-    std::vector<double> hannWindow = generateHannWindow(frameSize);
-    const int numframes = (signal.size() - frameSize) / hopSize + 1;
-    std::vector<std::vector<cd>> spec;
-    spec.reserve(numframes);
+std::vector<std::vector<float>> signalFramer(const std::vector<float>& signal,const int frameSize,const int hopSize) {
+
+    const size_t numframes = (signal.size() - frameSize) / hopSize + 1;
+    const int halfSize = frameSize / 2 +1;
+    std::vector<std::vector<float>> powerSpec;
+    powerSpec.reserve(numframes);
      for (int start = 0;start+frameSize<=signal.size();start+=hopSize) {
-       std::vector<cd> output(frameSize,0.0);
+       std::vector<cf> output(frameSize,0.0f);
         for (int i = 0; i < frameSize; ++i) {
-            output[i] = signal[start+i];
+            output[i] = static_cast<float>(signal[start+i]);
         }
        for (int j = 0;j < frameSize; ++j) {
-        output[j]*=hannWindow[j];
+        output[j].real *= static_cast<float>(hannWindow1024[j]);
        }
          iterativeFFT(output);
-         spec.push_back(output);
+         std::vector<float> power_frame(halfSize);
+         for (int p=0;p<halfSize;p++) {
+             const float re = output[p].real;
+             const float im = output[p].imag;
+             power_frame[p] = re*re+im*im;
+         }
+         powerSpec.push_back(power_frame);
    }
-    return spec;
+    return powerSpec;
 }
