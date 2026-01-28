75093934418ca395de403bef5a767ed54cc3a9bc
E.Roshan Manuel <roshanmanuel05@gmail.com>
2026-01-28 23:20:34 +0530

    new downsampler header
    changed accordingly

--- downsampler.h
+++ downsampler.h
@@ -1,11 +1,20 @@
-#ifndef DOWNSAMPLER_H
-#define DOWNSAMPLER_H
+#ifndef SHAZAM_DOWNSAMPLER_H
+#define SHAZAM_DOWNSAMPLER_H
 #include <vector>
-#include <complex>
-using cd = std::complex<double>;
-std::vector<double> lowpassfir(int,int,int);
-std::vector<double> polyphaseDecimate(const std::vector<double>&,const std::vector<double>&,int);
-std::vector<double> generateHannWindow(int);
-void iterativeFFT(std::vector<cd>&);
-std::vector<std::vector<cd>> signalFramer(const std::vector<double>&,const int,const int);
+#include <array>
+#include "complexf.h"
+constexpr float PI  =   3.14159265358979323846f;
+constexpr int stages = 10;
+constexpr int levels = 512;
+constexpr int windowSize = 1024;
+constexpr int filterTaps = 65;
+constexpr cf twiddle(const int ,const int );
+inline constexpr std::array<std::array<cf,levels>,stages> buildTable();
+inline constexpr std::array<float,windowSize> generateHannWindowforSTFT();
+inline constexpr std::array<float,65> generateHannWindowforLPF();
+std::vector<float> lowpassfir(int,int,int);
+std::vector<float> polyphaseDecimate(const std::vector<float>&,const std::vector<float>&,int);
+std::vector<float> generateHannWindow(int);
+void iterativeFFT(std::vector<cf>&);
+std::vector<std::vector<float>> signalFramer(const std::vector<float>&,const int,const int);
 #endif
